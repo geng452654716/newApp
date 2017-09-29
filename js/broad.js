7b6ec4e57069b37013fdd10405cfbe7d1cc6d471
452654716 <452654716@qq.com>
2017-09-29 09:46:25 +0800

    9.29

--- js/broad.js
+++ js/broad.js
@@ -1,17 +1,11 @@
 $(function(){
-	$(document).on("click",".bao",function(){
-		var name=$(this).html();
-		if(name=="立即报名"){
-			window.location.href="../index/sign.html"
-		}
-	})
-
 	let id = '' + window.location.search.substr(1).split('=')[1];
 
 	ajax('app.php/app_live_details',function(data){
 		if(data.rt === 1){
 			let Data = data.data;
 			let money = Data.data.price;
+			let endMoney = Data.data.start_price;
 			money = money.replace(/^(\d+)\.(\d+)/,function($0,$1,$2){
 				if($1 === '0' && $2 === '00'){
 					return '免费';
@@ -23,7 +17,20 @@ $(function(){
 					return `<a class="small">¥</a>${$1}.<span style='font-size:${28/50}rem'>${$2}</span>`;
 				}
 			})
-			console.log(money);
+
+			if(endMoney){
+				endMoney = endMoney.replace(/^(\d+)\.(\d+)/,function($0,$1,$2){
+					if($1 !== '0' && $2 ==='00'){
+						return '- <a class="small">¥</a>' + $1;
+					}
+					if($1 !== '0' && $2 !== '00'){
+						return `- <a class="small">¥</a>${$1}.<span style='font-size:${28/50}rem'>${$2}</span>`;
+					}
+				})
+			}else{
+				endMoney = '';
+			}
+
 			let str = `
 				<div class="banner"><img src="${imgUrl + Data.data.logo}"></div>
 				<div class="bro_tit">课程信息
@@ -35,12 +42,12 @@ $(function(){
 						</div>
 						<div class="int_p">
 							<p>${Data.teacher.name}</p>
-							<span>套课：5课时</span>
+							<span>套课：${Data.data.count}课时</span>
 						</div>
 						<div class="int_foot">
 							<div class="int_zi">
 								${money}
-							
+								${endMoney}
 							</div>
 							<div class="int_peo">1000人报名</div>
 						</div>
@@ -48,9 +55,65 @@ $(function(){
 					</div>
 					<div class="int_img"><img src="${imgUrl + Data.data.course_desc}"></div>
 				</ul>
-				<div class="bao">${Data.data.button}</div>
 			`
+			$('.bao').html(`${Data.data.ispay === 2?'立即报名':Data.data.button}`)
 			$('.content').html(str);
+			//付款弹框
+			let bombBox = $('.bombBox')[0];
+			var payment = bombBox.getElementsByClassName('payment');
+			payment = [...payment];
+			payment.forEach((e,i) => {
+				if(i === 0){
+					$(e).find('strong').html(`￥${Data.data.price}`)
+				}else if(i === 1){
+					$(e).find('strong').html(`${Data.data.start_price?`￥${Data.data.start_price}`:'￥0.00'}`)
+				}
+				$(e).mobileClick(function(e){
+					$('.payment').removeClass('active');
+					$(this).addClass('active');
+				})
+			})
+			//报名点击
+			$('.bao').mobileClick(function(e){
+				if(Data.data.ispay === 2){
+					$('.mask').show();
+					$('.bombBox').css({
+						bottom:25/50 + 'rem'
+					});
+				}
+				if(Data.data.button === '精彩回放'){
+					window.location.href = 'prespPlay.html?class_id='+data.data.Catalogdata[0].id;
+				}else if(Data.data.button === '直播中'){
+
+				}else if(Data.data.button === '即将开始'){
+
+				}
+			})
 		}
 	},{id},'post')
+	changeYouHui();
+	var you = true;
+	$('.youhui').mobileClick(function(){
+		you = !you;
+		changeYouHui();
+	})
+	$('.mask').mobileClick(function(){
+		$('.mask').hide();
+		$('.bombBox').css({
+			bottom:'-19.6rem'
+		});
+	})
+	function changeYouHui(){
+		if(you){
+			$('.button').addClass('quan').removeClass('weixin');
+			$('.youhui').addClass('yes').removeClass('no');
+			$('.button').html('优惠券支付');
+			$('.youhui strong').html('128元优惠券');
+		}else{
+			$('.button').addClass('weixin').removeClass('quan');
+			$('.youhui').addClass('no').removeClass('yes');
+			$('.button').html('微信支付');
+			$('.youhui strong').html('不使用优惠券');
+		}
+	}
 })
